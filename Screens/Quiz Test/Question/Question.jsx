e296c27e894bae8fdf57a549f66285334f42c73a
Hiep Nguyen <hiepnguyentpc@gmail.com>
2022-06-20 01:32:18 +0700

    Submit answer Done

--- Screens/Quiz Test/Question/Question.jsx
+++ Screens/Quiz Test/Question/Question.jsx
@@ -1,65 +1,131 @@
-
-
-import { View, Text, TouchableOpacity } from "react-native"
-import styles from "./styles"
-
+import { View, Text, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
 
 const Question = (props) => {
+  //const [accessToken, setAccessToken] = useState("")
+  const id = props.id;
+  const [answer, setAnswer] = useState("");
 
-    
-    
+  /*
+    const storeAccessToken = async () => {
+        try {
+          setAccessToken(await AsyncStorage.getItem("accessToken"));
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      storeAccessToken();
+      */
+  /*
+    if (answer){
+        console.log(answer)
+    }
 
+    if (id){
+        console.log(id)
+    }
 
-    return(
-        <View style={{backgroundColor:"white", height:"100%"}}>
-            <Text style={{marginTop: 40,fontSize:20, fontWeight:"bold", alignSelf:"center", marginBottom:40,}}>{props.question}</Text>
+    */
 
+  var postData = {
+    id: id,
+    correctanswer: answer,
+  };
 
+  const accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MmEwNzFlNWQyNjI1MDAwMjEwMzk0YjUiLCJpYXQiOjE2NTU2NjMzMDAsImV4cCI6MTY1NTY2MzQ4MCwidHlwZSI6ImFjY2VzcyJ9.aDUjqjYlGmJHgGW10GSJkMl97ifA25HgLZdDsmDS8RQ";
+  const url = "https://fwa-ec-quiz-mock1.herokuapp.com/v1/questions/submit";
+  const submitAnswer = () => {
+    axios
+      .post(
+        url,
+        { headers: { Authorization: `Bearer ${accessToken}` } },
+        {
+          data: [
+            {
+              id: "62a066dcd26250002103873f",
+              correctanswer: "Mị Châu",
+            },
+          ],
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  //submitAnswer()
 
-            <TouchableOpacity style={styles.touchableOp}
-                onPress={() => alert("True")}
-            >
-                <View style={styles.circle}>
-                    <Text style={styles.option}>A</Text>
-                </View>
-                <Text style={styles.answerText}>{props.answer1}</Text>
-            </TouchableOpacity>
-
-
-
-            <TouchableOpacity style={styles.touchableOp}
-                onPress={() => alert("False")}>
-                <View style={styles.circle}>
-                    <Text style={styles.option}>B</Text>
-                </View>
-                <Text style={styles.answerText}>{props.answer2}</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.touchableOp}
-            onPress={() => alert("False")}>
-                <View style={styles.circle}>
-                    <Text style={styles.option}>C</Text>
-                </View>
-                <Text style={styles.answerText}>{props.answer3}</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.touchableOp}
-            onPress={() => alert("False")}>
-                <View style={styles.circle}>
-                    <Text style={styles.option}>D</Text>
-                </View>
-                <Text style={styles.answerText}>{props.answer4}</Text>
-            </TouchableOpacity>
+  return (
+    <View style={{ backgroundColor: "white", height: "100%" }}>
+      <Text
+        style={{
+          marginTop: 40,
+          fontSize: 20,
+          fontWeight: "bold",
+          alignSelf: "center",
+          marginBottom: 40,
+        }}
+      >
+        {props.question}
+      </Text>
+      <TouchableOpacity
+        style={styles.touchableOp}
+        onPress={() => {
+          setAnswer(props.answer1);
+          submitAnswer();
+        }}
+      >
+        <View style={styles.circle}>
+          <Text style={styles.option}>A</Text>
         </View>
-    )
-}
+        <Text style={styles.answerText}>{props.answer1}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.touchableOp}
+        onPress={() => {
+          setAnswer(props.answer2);
+          submitAnswer();
+        }}
+      >
+        <View style={styles.circle}>
+          <Text style={styles.option}>B</Text>
+        </View>
+        <Text style={styles.answerText}>{props.answer2}</Text>
+      </TouchableOpacity>
 
-export default Question
+      <TouchableOpacity
+        style={styles.touchableOp}
+        onPress={() => {
+          setAnswer(props.answer3);
+          submitAnswer();
+        }}
+      >
+        <View style={styles.circle}>
+          <Text style={styles.option}>C</Text>
+        </View>
+        <Text style={styles.answerText}>{props.answer3}</Text>
+      </TouchableOpacity>
 
+      <TouchableOpacity
+        style={styles.touchableOp}
+        onPress={() => {
+          setAnswer(props.answer4);
+          submitAnswer();
+        }}
+      >
+        <View style={styles.circle}>
+          <Text style={styles.option}>D</Text>
+        </View>
+        <Text style={styles.answerText}>{props.answer4}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-/*
-            <TouchableOpacity style={{height:50, width:195, borderRadius:10, backgroundColor:"#1750AC", alignSelf:"center", alignItems:"center",justifyContent:"center", marginTop:80}}>
-                <Text style={{fontWeight:"bold", fontSize:16, color:"white",}}>Submit</Text>
-
-            </TouchableOpacity>
-        */
+export default Question;
--- Screens/Quiz Test/QuizMain.js
+++ Screens/Quiz Test/QuizMain.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, StyleSheet, ListRenderItem } from "react-native";
 import { Tabs } from "react-native-collapsible-tab-view";
-import Questions from '/Users/hiepnnguyen/Downloads/ECMock1-main/assets/questions.json'
 
 import axios from "axios";
 import { useState } from "react";
@@ -12,7 +11,9 @@ const HEADER_HEIGHT = 250;
 import HeaderQuizMain from "./Header/Header";
 import Question from "./Question/Question";
 
+
 const QuizMain = () => {
+
   const getBackPressed = () => {
     console.log("hello");
   };
@@ -20,6 +21,7 @@ const QuizMain = () => {
 
   const [accessToken, setAccessToken] = useState("")
   const [question, setQuestion] = useState("")
+
   const storeAccessToken = async () => {
     try {
       setAccessToken(await AsyncStorage.getItem("accessToken"));
@@ -30,7 +32,7 @@ const QuizMain = () => {
   storeAccessToken();
 
 
-  const getQuestionUser = async () => {
+  const getQuestionUser =  () => {
     axios.get(
       "https://fwa-ec-quiz-mock1.herokuapp.com/v1/questions?page=1&limit=8",
       { headers: {"Authorization" : `Bearer ${accessToken}`} }
@@ -42,38 +44,9 @@ const QuizMain = () => {
       console.log(error);
     });
   };
-  getQuestionUser()
-
-
-  console.log(question[0])
-
-  
-   const q1 = "null" || question[0]
-   const id1 = "null" ||q1["id"]
-
-   const q2 = "null" || question[1]
-   const id2 = "null" ||q2["id"]
-
-   const q3 = "null" ||question[2]
-   const id3 = "null" ||q3["id"]
-
-   const q4 = "null" ||question[3]
-   const id4 = "null" ||q4["id"]
-
-   const q5 = "null" ||question[4]
-   const id5 = "null" ||q5["id"]
-
-   const q6 = "null" ||question[5]
-   const id6 = "null" ||q6["id"]
-
-   const q7 = "null" ||question[6]
-   const id7 = "null" ||q7["id"]
-
-   const q8 = "null" || question[7]
-   const id8 = "null" ||q8["id"]
-   //console.log(id8)
-   
-
+  if (!question){
+    getQuestionUser()
+  }
   return (
     <Tabs.Container
       renderHeader={HeaderQuizMain}
@@ -81,7 +54,7 @@ const QuizMain = () => {
     >
       <Tabs.Tab name="1">
         <Tabs.ScrollView>
-          <Question question={question[0]?.["question"]} answer1={question[0]?.["answer1"]} answer2={question[0]?.["answer2"]} answer3={question[0]?.["answer3"]} answer4={question[0]?.["answer4"]}/>
+          <Question question={question[0]?.["question"]} id ={question[0]?.["id"]} answer1={question[0]?.["answer1"]} answer2={question[0]?.["answer2"]} answer3={question[0]?.["answer3"]} answer4={question[0]?.["answer4"]}/>
         </Tabs.ScrollView>
       </Tabs.Tab>
 
